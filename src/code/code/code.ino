d10d0a472a7141940b3e123cf41d3dc5dab2f125
TheCoder <107439786+musarda@users.noreply.github.com>
2023-08-20 13:08:10 +0300

    Update code.ino

--- src/code/code/code.ino
+++ src/code/code/code.ino
@@ -8,23 +8,22 @@ LiquidCrystal lcd(0, 1, 8, 9, 10, 11);
 void setup() {
   lcd.begin(16,2);
   lcd.setCursor(0,0);
-  lcd.print("   Sicaklik  ");
+  lcd.print("   Heat  ");
   lcd.setCursor(0,1);
-  lcd.println(" Hesaplaniyor ");
+  lcd.println(" Calculating ");
   delay(5000);
 
 
 }
 
 void loop() {
-  int celsius_deger = (5.0 * analogRead(lm35Pin) * 100.0) / 6000;  // oynama yapılabilir.
+  int celsius_value = (5.0 * analogRead(lm35Pin) * 100.0) / 6000;
   lcd.clear(); 
   lcd.setBacklight(LOW);
   lcd.setCursor(0,0);
-  lcd.print("   Robotistan");
   lcd.setCursor(0,1);
-  lcd.print(" Sicaklik: "); 
-  lcd.print(celsius_deger);
+  lcd.print(" Heat: "); 
+  lcd.print(celsius_value);
   lcd.print((char)223);
   lcd.print("C");
   delay(2000);
